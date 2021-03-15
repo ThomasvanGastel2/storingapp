8be86f1248ddba81b022642504ff267756659e0d
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-03-15 13:14:38 +0100

    Create inline.php

---
+++ inline.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>De krant</title>
+</head>
+<body>
+
+    <?php
+    $title = "Brandweer redt kat uit boom";
+    $date = "1 feb 2021";
+    $contents = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quo maxime est explicabo odit, possimus odio enim molestias! Neque dolorem harum provident iste tempore veniam consectetur doloribus mollitia illo itaque.";
+    ?>
+
+    <h1><?php echo	$title; ?></h1>
+    <p><em>Datum: <?php echo $date; ?></em></p>
+    <p></p>
+
+</body>
+</html>
